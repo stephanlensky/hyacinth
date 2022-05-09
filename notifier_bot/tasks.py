fe0b45028b1d3542b7ed5941a7dddbbe9685f933
Stephan Lensky <stephanl.public@gmail.com>
2022-05-09 22:12:58 +0000

    Cache sources between task runs

--- notifier_bot/tasks.py
+++ notifier_bot/tasks.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 from datetime import datetime
+from functools import cache
+
+from celery import Task
 
 from notifier_bot.celery import app
 from notifier_bot.db.listing import save_listings as save_listings_to_db
@@ -11,24 +14,27 @@ from notifier_bot.sources.craigslist import CraigslistSearchParams, CraigslistSo
 _logger = logging.getLogger(__name__)
 
 
-def _make_source(search_spec: SearchSpec) -> ListingSource:
-    if search_spec.source == SearchSpecSource.CRAIGSLIST:
-        if not isinstance(search_spec.search_params, CraigslistSearchParams):
-            raise ValueError(
-                "Search params for Craigslist source has incorrect type"
-                f" {type(search_spec.search_params)}"
-            )
-        return CraigslistSource(search_spec.search_params)
+class SearchTask(Task):  # pylint: disable=abstract-method
+    @cache
+    def make_source(self, search_spec: SearchSpec) -> ListingSource:
+        _logger.debug(f"Instantiating new source for {search_spec}")
+        if search_spec.source == SearchSpecSource.CRAIGSLIST:
+            if not isinstance(search_spec.search_params, CraigslistSearchParams):
+                raise ValueError(
+                    "Search params for Craigslist source has incorrect type"
+                    f" {type(search_spec.search_params)}"
+                )
+            return CraigslistSource(search_spec.search_params)
 
-    raise NotImplementedError(f"{search_spec.source} not implemented")
+        raise NotImplementedError(f"{search_spec.source} not implemented")
 
 
-@app.task
+@app.task(base=SearchTask)
 def get_and_save_listings(search_spec_json: str, after_time_isoformat: str) -> None:
     search_spec = SearchSpec.parse_raw(search_spec_json)
     after_time = datetime.fromisoformat(after_time_isoformat)
 
-    source = _make_source(search_spec)
+    source = get_and_save_listings.make_source(search_spec)
     _logger.info(
         f"Starting task to get {search_spec.source} listings since {after_time} for"
         f" search_params={search_spec.search_params}"
