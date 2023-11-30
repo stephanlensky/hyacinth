ea2edd7b04030d502d3301848a52bf74b9bb3b41
Stephan Lensky <public@slensky.com>
2023-11-30 05:08:25 +0000

    Remove unused import

--- tests/hyacinth/test_filters.py
+++ tests/hyacinth/test_filters.py
@@ -1,4 +1,4 @@
-from hyacinth.filters import _apply_string_rule_expr, parse_string_rule_expr
+from hyacinth.filters import _apply_string_rule_expr
 
 
 def test__apply_string_rule__some_rule_expression__applies_rule_correctly() -> None:
